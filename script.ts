11386d6a2834eb0132b212185e933798fe8f1ce5
AhsanMalik5 <159904465+AhsanMalik5@users.noreply.github.com>
2024-12-09 23:56:52 +0500

    first

---
+++ script.ts
@@ -0,0 +1,20 @@
+export const myFunction = () => {
+  const toggleButton = document.getElementById("toggle-skills");
+  const skillsSection = document.getElementById("skills");
+
+  if (skillsSection && toggleButton) {
+    toggleButton.addEventListener("click", () => {
+      if (skillsSection.style.display === "none") {
+        skillsSection.style.display = "block";
+      } else {
+        skillsSection.style.display = "none";
+      }
+    });
+  } else {
+    console.error("Button or Skills section not found");
+  }
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+  myFunction();
+});
